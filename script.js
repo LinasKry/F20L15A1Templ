f14d5fed4e53e9d64ffa16607f8cb8b97c2ccd23
LinasKry <linas.kryzevicius@gmail.com>
2022-05-16 01:16:06 +0300

    init

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var tabai = document.getElementsByClassName("tab");
-var buttons = document.getElementsByTagName("button");
 
 for (var visiems of tabai) {
   visiems.style.display = "none";
@@ -8,6 +7,42 @@ for (var visiems of tabai) {
 function changes(event, id) {
   for (var visiems of tabai) {
     visiems.style.display = "none";
+    visiems.classList.remove("animacija");
   }
   document.getElementById(id).style.display = "block";
+
+  setTimeout(function () {
+    document.getElementById(id).classList.add("animacija");
+  }, 100);
 }
+
+//////
+// var pics = document.getElementsByClassName("pic");
+// var tabai = document.getElementsByClassName("tab");
+
+// for (var visiems of pics) {
+//   visiems.style.display = "none";
+// }
+// for (var visiems of tabai) {
+//   visiems.style.display = "none";
+// }
+
+// function changes(event, id) {
+//   for (var visiems of pics) {
+//     visiems.style.display = "none";
+//     visiems.classList.remove("animacija");
+//   }
+//   document.getElementById(id).style.display = "block";
+
+//   function changes(event, id) {
+//     for (var visiems of tabai) {
+//       visiems.style.display = "none";
+//       visiems.classList.remove("animacija");
+//     }
+//     document.getElementById(id).style.display = "block";
+//   }
+
+//   setTimeout(function () {
+//     document.getElementById(id).classList.add("animacija");
+//   }, 100);
+// }
